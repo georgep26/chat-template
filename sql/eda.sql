2ee41b9552a9dec35e93d6cb1df06fc70963f3c0
George <george.padavick@gmail.com>
2025-12-26 08:47:02 -0700

    feat: add ordering and count query to chat_history in EDA SQL script

--- sql/eda.sql
+++ sql/eda.sql
@@ -2,3 +2,8 @@ select * from bedrock_integration.bedrock_kb limit 10;
 
 select * from chat_history ch limit 10;
 
+select * from chat_history ch 
+order by ch.created_at desc
+
+select count(ch.id) 
+from chat_history ch 
